7c1212473bc583861bfab645e23a202e9fa1695a
Weidong Xu <weidxu@microsoft.com>
2022-04-20 11:25:23 +0800

    use VM sample

--- src/main/java/com/fabrikam/App.java
+++ src/main/java/com/fabrikam/App.java
@@ -2,11 +2,15 @@ package com.fabrikam;
 
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.policy.HttpLogDetailLevel;
-import com.azure.identity.EnvironmentCredentialBuilder;
-import com.azure.identity.AzureAuthorityHosts;
-import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.AzureAuthorityHosts;
+import com.azure.identity.EnvironmentCredentialBuilder;
 import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.compute.models.KnownLinuxVirtualMachineImage;
+import com.azure.resourcemanager.compute.models.VirtualMachine;
+import com.azure.resourcemanager.compute.models.VirtualMachineSizeTypes;
 
 /**
  * Hello world!
@@ -14,25 +18,43 @@ import com.azure.resourcemanager.AzureResourceManager;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
 
-        TokenCredential credential = new EnvironmentCredentialBuilder()
-        .authorityHost(AzureAuthorityHosts.AZURE_PUBLIC_CLOUD)
-        .build();
+        final String userName = "YOUR_VM_USERNAME";
+        final String sshKey = "YOUR_PUBLIC_SSH_KEY";
 
-        // If you don't set the tenant ID and subscription ID via environment variables,
-        // change to create the Azure profile with tenantId, subscriptionId, and Azure environment.
-        AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+        try {
+            TokenCredential credential = new EnvironmentCredentialBuilder()
+                    .authorityHost(AzureAuthorityHosts.AZURE_PUBLIC_CLOUD)
+                    .build();
 
-        AzureResourceManager azureResourceManager = AzureResourceManager.configure()
-                .withLogLevel(HttpLogDetailLevel.BASIC)
-                .authenticate(credential, profile)
-                .withDefaultSubscription();
+            // If you don't set the tenant ID and subscription ID via environment variables,
+            // change to create the Azure profile with tenantId, subscriptionId, and Azure environment.
+            AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
 
+            AzureResourceManager azureResourceManager = AzureResourceManager.configure()
+                    .withLogLevel(HttpLogDetailLevel.BASIC)
+                    .authenticate(credential, profile)
+                    .withDefaultSubscription();
 
-        System.out.println("count " + azureResourceManager.storageAccounts().list().stream().count());
+            // Create an Ubuntu virtual machine in a new resource group.
+            VirtualMachine linuxVM = azureResourceManager.virtualMachines().define("testLinuxVM")
+                    .withRegion(Region.US_EAST)
+                    .withNewResourceGroup("sampleVmResourceGroup")
+                    .withNewPrimaryNetwork("10.0.0.0/24")
+                    .withPrimaryPrivateIPAddressDynamic()
+                    .withoutPrimaryPublicIPAddress()
+                    .withPopularLinuxImage(KnownLinuxVirtualMachineImage.UBUNTU_SERVER_16_04_LTS)
+                    .withRootUsername(userName)
+                    .withSsh(sshKey)
+                    .withUnmanagedDisks()
+                    .withSize(VirtualMachineSizeTypes.STANDARD_D3_V2)
+                    .create();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
 
         System.exit(0);
     }
